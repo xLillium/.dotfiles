9e28f6a11cb77cc5f4151b04bd1ba00c51803349
xLillium <nicolas.motillon@gmail.com>
2023-07-24 00:40:27 +0200

    Add which-key with default conf

--- nvim/.config/nvim/lua/og/plugins.lua
+++ nvim/.config/nvim/lua/og/plugins.lua
@@ -85,6 +85,18 @@ return require("packer").startup(function(use)
 	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+	use({
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
